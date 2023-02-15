8d34c4fd47e96d5c7306f7aa77fa87372ea5fa0f
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2023-02-15 19:04:43 +0100

    hcer base de datos y visualización de la misma

--- app/src/main/java/com/lisbeth/lightoutgame/Game.java
+++ app/src/main/java/com/lisbeth/lightoutgame/Game.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 
+import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -14,8 +20,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Game extends AppCompatActivity {
@@ -28,7 +37,22 @@ public class Game extends AppCompatActivity {
     private Button[][] buttons;
     private Drawable drawableOn;
     private Drawable drawableOff;
+    private Drawable drawableTouch;
+    private int segundos = 0;
+    private TextView textTime;
     private GameModel model;
+    private Handler handler = new Handler();
+    private Button solucionar;
+    private List<Pair<Integer, Integer>> lucesAMostrar;
+
+
+    private Runnable runnable = new Runnable() {
+        public void run() {
+            segundos++;
+            textTime.setText(String.valueOf(segundos));
+            handler.postDelayed(this, 1000); // Ejecutar de nuevo el Runnable en un segundo
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +60,10 @@ public class Game extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         this.setDrawableOff(getResources().getDrawable(R.drawable.light_off));
         this.setDrawableOn(getResources().getDrawable(R.drawable.light_on));
-
-        // tomamos los valores de la pantalla inicial para crear la matriz de botones
+        this.setDrawableTouch(getResources().getDrawable(R.drawable.light_touch));
+        this.textTime = findViewById(R.id.time);
+        this.solucionar = findViewById(R.id.botonSolucion);
+        //* tomamos los valores de la pantalla inicial para crear la matriz de botones
         if (getIntent().hasExtra("width") || getIntent().hasExtra("height")) {
             this.width = getIntent().getIntExtra("width", 5);
             this.height = getIntent().getIntExtra("height", 5);
@@ -45,6 +71,7 @@ public class Game extends AppCompatActivity {
             throw new IllegalArgumentException("Activity cannot find  extras " + "width");
         }
         this.gameLayout();
+        this.model = new GameModel(height, width);
         this.inicializarTablero();
         // agregamos los listeners
         for (int i = 0; i < buttons.length; i++) {
@@ -53,9 +80,97 @@ public class Game extends AppCompatActivity {
             }
         }
 
+        // * inicualizamos el contador de segundos
+        handler.post(runnable);
+
+
+        solucionar.setOnClickListener(new View.OnClickListener() {
+            //Handler timer = new Handler();
+
+            @Override
+            public void onClick(View v) {
+                    //handler.removeCallbacks(runnable);
+                    mostrarLucesATocar();
+
+            }
+        });
+
+
+    }
+
+    private void mostrarLucesATocar() {
+        lucesAMostrar = obtenerLucesAMostrar(); // Obtener la lista de luces a mostrar
+
+        Handler timer = new Handler();
+        for (int i = 0; i < lucesAMostrar.size(); i++) {
+            Log.d("solsssss", "aquii");
+            final Pair<Integer, Integer> luz = lucesAMostrar.get(i);
+            timer.postDelayed(new Runnable() {
+                public void run() {
+                    mostrarLuz(luz.first, luz.second);
+
+                }
+            }, i * 1000); // Mostrar la luz con una pausa de 1 segundo
+            //cambiarRojos();
+        }
+    }
+
+    private void mostrarLuz(int fila, int columna) {
+       // cambiarRojos();
+        buttons[fila][columna].setText("TOUCH");
+        buttons[fila][columna].setTextColor(Color.RED);
+        Button boton = buttons[fila][columna];
+        Log.d("sol", "aquii");
+        updateAdjacentButtons(fila, columna);
+
+
     }
 
 
+    private List<Pair<Integer, Integer>> obtenerLucesAMostrar() {
+        List<Pair<Integer, Integer>> luces = new ArrayList<>();
+
+        for (int fila = 0; fila < model.getSolution().length; fila++) {
+            for (int columna = 0; columna < model.getSolution()[fila].length; columna++) {
+                if (model.getSolution()[fila][columna]) {
+                    luces.add(new Pair<>(fila, columna));
+                }
+            }
+        }
+
+        return luces;
+    }
+
+
+    public void cambiarRojos(){
+        for (int i = 0; i < buttons.length; i++) {
+
+            for (int j = 0; j < buttons[i].length; j++) {
+
+                if (buttons[i][j].getBackground() == drawableTouch){
+                    buttons[i][j].setBackground(drawableOff);
+
+                }
+            }
+        }
+    }
+
+    public void changeButtonBackground(final Button button) {
+
+        int fila = this.obtenerFilaBoton(button);
+        int columna = this.obtenerColumnaBoton(button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateAdjacentButtons(fila, columna);
+                finalizado();
+            }
+        });
+
+    }
+
+
+ 
     public void gameLayout(){
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
@@ -92,21 +207,8 @@ public class Game extends AppCompatActivity {
             }
             root.addView(rowLayout);
         }
-    }
-
-    public void changeButtonBackground(final Button button) {
-
-    int fila = this.obtenerFilaBoton(button);
-    int columna = this.obtenerColumnaBoton(button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                actualizarBotonesAdyacentes(fila, columna);
-            }
-        });
 
     }
-
 
     private int obtenerFilaBoton(Button boton) {
         for (int fila = 0; fila < buttons.length; fila++) {
@@ -131,7 +233,6 @@ public class Game extends AppCompatActivity {
     }
 
 
-
     public Button[][] getButtons() {
         return buttons;
     }
@@ -148,6 +249,14 @@ public class Game extends AppCompatActivity {
         return drawableOff;
     }
 
+    public Drawable getDrawableTouch() {
+        return drawableTouch;
+    }
+
+    public void setDrawableTouch(Drawable drawableTouch) {
+        this.drawableTouch = drawableTouch;
+    }
+
     public void setDrawableOff(Drawable drawableOff) {
         this.drawableOff = drawableOff;
     }
@@ -161,40 +270,60 @@ public class Game extends AppCompatActivity {
         }
     }
 
-
-    private void actualizarBotonesAdyacentes(int fila, int columna) {
-        cambiarColorBoton(buttons[fila][columna]);
-        // Cambiar el color del botón de arriba
-        if (fila > 0) {
-            cambiarColorBoton(buttons[fila - 1][columna]);
-        }
-        // Cambiar el color del botón de abajo
-        if (fila < buttons.length - 1) {
-            cambiarColorBoton(buttons[fila + 1][columna]);
-        }
-        // Cambiar el color del botón de la izquierda
-        if (columna > 0) {
-            cambiarColorBoton(buttons[fila][columna - 1]);
-        }
-        // Cambiar el color del botón de la derecha
-        if (columna < buttons[fila].length - 1) {
-            cambiarColorBoton(buttons[fila][columna + 1]);
-        }
-    }
-
-
     public void inicializarTablero(){
         Random ran = new Random();
-
         for (int i = 0; i < buttons.length; i++) {
+
             for (int j = 0; j < buttons[i].length; j++) {
                 int probabilidad = ran.nextInt(2);
                 if (probabilidad==1){
-                    buttons[i][j].setBackground(drawableOn);
+                    updateAdjacentButtons(i, j);
+
                 }
             }
         }
+
+
     }
+
+
+
+
+
+    public void updateAdjacentButtons( int row, int col) {
+        this.model.getSolution()[row][col] = !model.getSolution()[row][col];
+
+        int numRows = buttons.length;
+        int numCols = buttons[0].length;
+
+        // Actualiza el botón al oeste
+        if (col > 0) {
+            Button westButton = buttons[row][col - 1];
+            cambiarColorBoton(westButton);
+        }
+
+        // Actualiza el botón al este
+        if (col < numCols - 1) {
+            Button eastButton = buttons[row][col + 1];
+            cambiarColorBoton(eastButton);
+        }
+
+        // Actualiza el botón al norte
+        if (row > 0) {
+            Button northButton = buttons[row - 1][col];
+            cambiarColorBoton(northButton);
+        }
+
+        // Actualiza el botón al sur
+        if (row < numRows - 1) {
+            Button southButton = buttons[row + 1][col];
+            cambiarColorBoton(southButton);
+        }
+    }
+
+
+
+
 
     public  boolean finalizado(){
         for (int i = 0; i < buttons.length; i++) {
@@ -204,8 +333,34 @@ public class Game extends AppCompatActivity {
                 }
             }
         }
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            long[] patron = { 500, 500 };
+            vibrator.vibrate(patron, -1);
+        }
+
         return  true;
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        handler.removeCallbacks(runnable); // Detener el contador
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
--- app/src/main/java/com/lisbeth/lightoutgame/GameModel.java
+++ app/src/main/java/com/lisbeth/lightoutgame/GameModel.java
@@ -2,33 +2,15 @@ package com.lisbeth.lightoutgame;
 
 public class GameModel {
     private boolean[][] board;
+    private boolean[][] solution;
 
-    public GameModel(int width, int height) {
-        this.board = new boolean[width][height];
+    public GameModel( int height, int width) {
+        this.board = new boolean[height][width];
+        this.solution = new boolean[height][width];
 
     }
 
-    public void changeLight(int row, int col) {
-        int rows = board.length;
-        int cols = board[0].length;
 
-        // Cambia el estado de la celda presionada
-        board[row][col] = !board[row][col];
-
-        // Cambia el estado de las celdas adyacentes
-        if (row > 0) {
-            board[row - 1][col] = !board[row - 1][col];
-        }
-        if (row < rows - 1) {
-            board[row + 1][col] = !board[row + 1][col];
-        }
-        if (col > 0) {
-            board[row][col - 1] = !board[row][col - 1];
-        }
-        if (col < cols - 1) {
-            board[row][col + 1] = !board[row][col + 1];
-        }
-    }
 
     public void randomBoard() {
         for (int i = 0; i < board.length; i++) {
@@ -46,5 +28,18 @@ public class GameModel {
         this.board = board;
     }
 
-    
+    public boolean[][] getBoard() {
+        return board;
+    }
+
+    public boolean[][] getSolution() {
+        return solution;
+    }
+
+    public void setSolution(boolean[][] solution) {
+        this.solution = solution;
+    }
+
+
+
 }
