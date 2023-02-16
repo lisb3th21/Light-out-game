3c77f73000bf4e4a53bd8ec04fa10c74d5cba7c0
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2023-02-16 14:54:10 +0100

    hacer las activitys faltantes

---
+++ app/src/main/java/com/lisbeth/lightoutgame/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.lisbeth.lightoutgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Configurar la vista
+        ImageView logo = findViewById(R.id.logo);
+        AnimationSet animationSet = new AnimationSet(true);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1, 1.5f, 1, 1.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(3000);
+        animationSet.addAnimation(scaleAnimation);
+        logo.startAnimation(animationSet);
+
+        // Configurar un temporizador para pasar a la siguiente actividad
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+    }
